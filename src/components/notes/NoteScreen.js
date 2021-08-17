ae9ac8dec797696e5bdb0b11324c1bba9b200b0c
Juan Patron <josejuanpatron1630@gmail.com>
2021-08-17 12:45:43 -0500

    Se agregaron los estilos del form de notes

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -6,6 +6,23 @@ export const NoteScreen = () => {
     return (
         <div  className="note-screen-content">
             <NoteAppBar />
+
+            <div className="notes__content" >
+                <input type="text" 
+                        className="notes__title-input"
+                        placeholder="an aweson titlfor everyone"
+                        autoComplete="off"
+                        />
+                <textarea 
+                    placeholder="What happend today"
+                    className="notes__textarea"
+                    >
+
+                </textarea>
+            </div>
+            <div  className="notes__image" >
+                <img src="http://2.bp.blogspot.com/-JanvBHsi2p4/VUSYPnvG9DI/AAAAAAAAFxk/jCixiE0gtJQ/s1600/silicon-valley-segunda-temporada-online.jpg" />
+            </div>
         </div>
     )
 }

a32ac8c7fb54a70c562a1cf195e600872363e8e4
Juan Patron <josejuanpatron1630@gmail.com>
2021-09-05 21:50:11 -0500

    Se iniciaron los test

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,25 @@
+import { types } from '../../types/types';
+
+describe('Pruebas con los tipos' , () => {
+
+    test('Debe de tener los mismos tipos' , () => {
+        expect( types ).toEqual({
+            loggin: '[auth] Login',
+            logout: '[auth] Logout',
+        
+            uiRemoveError: '[UI] Remove Error',
+            uiSetError: '[UI] Set Error',
+        
+            uiStartLoading : '[UI] Start Loading',
+            uiFinishLoading : '[UI] Finish Loading',
+        
+            notesAddNew : '[Notes] New note',
+            notesActive : '[Notes] Set active note',
+            notesLoad : '[Notes] Load notes',
+            notesUpdated : '[Notes] Updated note',
+            notesFileUrl : '[Notes] Updated image url',
+            notesDelete : '[Notes] Delete note',
+            notesLogoutCleaning : '[Notes] Logout Cleaning',
+        })
+    });
+});

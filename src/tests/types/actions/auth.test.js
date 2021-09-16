8bdd1db621c823be0a2cf817a8f4e8e771b6c6cd
Juan Patron <josejuanpatron1630@gmail.com>
2021-09-15 22:42:40 -0500

    Se hizo el test de login y del logout

--- src/tests/types/actions/auth.test.js
+++ src/tests/types/actions/auth.test.js
@@ -1,10 +1,29 @@
-
+import { login, logOut } from '../../../actions/auth'
+import { types } from '../../../types/types';
 
 
 describe('Pruebas en las acciones del auth', () => {
-    
+
+        const uid = 'ABC123';
+        const displayName = 'Juan Patrón'
     
     test('Login y logout deben de crear las acciones necesarias' , () => {
-        
+
+        const loginAction = login(uid, displayName);
+        const logOutAction = logOut();
+
+        expect(loginAction).toEqual({
+            type: types.loggin,
+            payload: {
+                uid,
+                displayName
+            }
+        });
+
+
+        expect(logOutAction).toEqual({
+            type: types.logout
+        })
+   
     });
 });
